00ffd3ec6cbd28c64dc60a478894c8c8df2798ea
miloszorzechowski <milosz.orzechowski@proton.me>
2023-03-11 20:42:41 +0100

    Corrected indentation

--- src/subject01/exercise2/FlySpeakTest.java
+++ src/subject01/exercise2/FlySpeakTest.java
@@ -2,37 +2,37 @@ package subject01.exercise2;
 
 class FlySpeakTest {
     public static void main(String[] args) {
-		var f = new Flyable[]{new Bird(), new Plane(), new UFO(), new Virus()};
-		var s = new Speakable[]{new Bird()};
+        var f = new Flyable[]{new Bird(), new Plane(), new UFO(), new Virus()};
+        var s = new Speakable[]{new Bird()};
 
-		System.out.println(shortest(f));
-		System.out.println(loudest(s));
+        System.out.println(shortest(f));
+        System.out.println(loudest(s));
     }
 
-	private static Flyable shortest(Flyable[] f) {
-		var shortestDistance = f[0].distance();
-		var object = f[0];
+    private static Flyable shortest(Flyable[] f) {
+        var shortestDistance = f[0].distance();
+        var object = f[0];
 
-		for (var index = 1; index < f.length; index++) {
-			if (f[index].distance() < shortestDistance) {
-				shortestDistance = f[index].distance();
-				object = f[index];
-			}
-		}
+        for (var index = 1; index < f.length; index++) {
+            if (f[index].distance() < shortestDistance) {
+                shortestDistance = f[index].distance();
+                object = f[index];
+            }
+        }
 
-		return object;
-	}
+        return object;
+    }
 
-	private static Speakable loudest(Speakable[] s) {
-		var loudestObject = s[0].speak().length();
-		var object = s[0];
+    private static Speakable loudest(Speakable[] s) {
+        var loudestObject = s[0].speak().length();
+        var object = s[0];
 
-		for (var index = 1; index < s.length; index++) {
-			if (s[index].speak().length() > loudestObject) {
-				object = s[index];
-			}
-		}
+        for (var index = 1; index < s.length; index++) {
+            if (s[index].speak().length() > loudestObject) {
+                object = s[index];
+            }
+        }
 
-		return object;
-	}
+        return object;
+    }
 }
